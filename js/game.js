220963473ec2762d804ab1c687ea40bcb9422d67
Jenna from the block! <jregiste@u.rochester.edu>
2016-08-24 11:06:22 -0400

    Some changes!

--- js/game.js
+++ js/game.js
@@ -22,98 +22,132 @@ stage.enableMouseOver();
 
 
 var game = function(){
-        var next = function(){
-                stage.removeAllChildren();
-stage.update();
-        //welcome
-                var text = new createjs.Text("Welcome to the", "bold 48px Arial", "white");
-                text.x=300;
-                text.y=10;
-                text.shadow = new createjs.Shadow("#000", 4, 4, 8);
-                stage.addChild(text);
-                var text2 = new createjs.Text("Concept Factory", "bold 70px Arial", "white");
-                text2.shadow = new createjs.Shadow("#000", 4, 4, 8);
-                stage.addChild(text2);
-                text2.x=225;
-                text2.y=80;
-                stage.update();
+        var text = new createjs.Text("Welcome to the", "bold 48px Arial", "white");
+        text.x=300;
+        text.y=10;
+        text.shadow = new createjs.Shadow("#000", 4, 4, 8);
+        
+        var text2 = new createjs.Text("Concept Factory", "bold 70px Arial", "white");
+        text2.shadow = new createjs.Shadow("#000", 4, 4, 8);
+        
+        text2.x=225;
+        text2.y=80;
+        stage.update();
 
-                //here is the "apply box"
-                var box = new createjs.Bitmap("images/metalbox.jpg");
-                box.scaleX = .50;
-                box.scaleY = .50;
+        //here is the "apply box"
+        var box = new createjs.Bitmap("images/metalbox.jpg");
+        box.scaleX = .50;
+        box.scaleY = .50;
+        box.x = 400;
+        box.y = 260;
+        
+        var gear = new createjs.Bitmap("images/gear.png");
+        gear.scaleX = .3;
+        gear.scaleY = .3;
+        
+        gear.regX=150;
+        gear.regY = 150;
+        
+        
+        var gear2 = new createjs.Bitmap("images/gear.png");
+        gear2.scaleX = .2;
+        gear2.scaleY = .2;
+        
+        gear2.regX=150;
+        gear2.regY = 150;
+        
+        var gear3 = new createjs.Bitmap("images/gear.png");
+        gear3.scaleX = .3;
+        gear3.scaleY = .3;
+        
+        gear3.regX=150;
+        gear3.regY = 150;
+        
+        
+      
+
+        //left robot arm
+        var left = new createjs.Bitmap("images/left.png");
+        left.scaleX = .50;
+        left.scaleY = .50;
+        left.x = 0;
+        left.y = 260;
+        
+
+        //right robot arm
+        var right = new createjs.Bitmap("images/right.png");
+        right.scaleX = .50;
+        right.scaleY = .50;
+        right.x = 680;
+        right.y = 260;
+        
+
+        //tile holder
+        var holder = new createjs.Bitmap("images/holder2.jpg");
+        holder.y=550;
+        holder.x=225;
+        
+        //all the tiles
+        var t1 = new createjs.Bitmap("images/s1.jpg");
+        
+        var t2 = new createjs.Bitmap("images/s2.jpg");
+        
+        var t3 = new createjs.Bitmap("images/s3.jpg");
+        
+        var t4 = new createjs.Bitmap("images/s7.jpg");
+        
+        var t5 = new createjs.Bitmap("images/s8.jpg");
+       
+        var t6 = new createjs.Bitmap("images/s10.jpg");
+        
+        
+        var next = function(){
+                
+                
+                stage.removeAllChildren();
+                stage.update();
+                stage.addChild(text);
+                stage.addChild(text2);
                 box.x = 400;
                 box.y = 260;
-                stage.addChild(box);
-                var gear = new createjs.Bitmap("images/gear.png");
-                gear.scaleX = .3;
-                gear.scaleY = .3;
                 gear.x=440;
                 gear.y = 290;
-                gear.regX=150;
-                gear.regY = 150;
-                
-                stage.addChild(gear);
-                var gear2 = new createjs.Bitmap("images/gear.png");
-                gear2.scaleX = .2;
-                gear2.scaleY = .2;
                 gear2.x=440;
                 gear2.y = 350;
-                gear2.regX=150;
-                gear2.regY = 150;
-                stage.addChild(gear2);
-                var gear3 = new createjs.Bitmap("images/gear.png");
-                gear3.scaleX = .3;
-                gear3.scaleY = .3;
                 gear3.x=575;
                 gear3.y = 420;
-                gear3.regX=150;
-                gear3.regY = 150;
-                
-                stage.addChild(gear3);
-              
-
-                //left robot arm
-                var left = new createjs.Bitmap("images/left.png");
-                left.scaleX = .50;
-                left.scaleY = .50;
-                left.x = 0;
-                left.y = 260;
-                stage.addChild(left);
-
-                //right robot arm
-                var right = new createjs.Bitmap("images/right.png");
-                right.scaleX = .50;
-                right.scaleY = .50;
-                right.x = 680;
-                right.y = 260;
-                stage.addChild(right);
-
-                //tile holder
-                var holder = new createjs.Bitmap("images/holder2.jpg");
-                holder.y=550;
-                holder.x=225;
-                stage.addChild(holder);
-
-                //all the tiles
-                var t1 = new createjs.Bitmap("images/s1.jpg");
                 t1.y=600;
                 t1.x=260;
-                var t2 = new createjs.Bitmap("images/s2.jpg");
+                t1.scaleX = t1.scaleY = 1.
                 t2.y=600;
                 t2.x=350;
-                var t3 = new createjs.Bitmap("images/s3.jpg");
+                t2.scaleX = t2.scaleY = 1.
                 t3.y=600;
                 t3.x=440;
-                var t4 = new createjs.Bitmap("images/s7.jpg");
+                t3.scaleX = t3.scaleY = 1.
                 t4.y=600;
                 t4.x=530;
-                var t5 = new createjs.Bitmap("images/s8.jpg");
+                t4.scaleX = t4.scaleY = 1.
                 t5.y=600;
                 t5.x=620;
-                var t6 = new createjs.Bitmap("images/s10.jpg");
+                t5.scaleX = t5.scaleY = 1.
                 t6.y=600;
                 t6.x=710;
+                t6.scaleX = t6.scaleY = 1.
+           
+                stage.addChild(box);
+                stage.addChild(gear);
+                stage.addChild(gear2);
+                stage.addChild(gear3);
+                stage.addChild(holder);
+                stage.addChild(right);
+                stage.addChild(left);
+                
+               
+        //welcome
+               
+
+               
 
                 var background = new createjs.Shape();
 			                background.name = "background";
@@ -134,6 +168,7 @@ stage.update();
                 button.on("click", handleClick);
                 
                 stage.addChild(button);
+                button.mouseEnabled = false;
                  
         //possible choices
                 var choices = [t1,t2,t3,t4,t5,t6]
@@ -181,7 +216,9 @@ stage.update();
 
 
                 //moving the arms, with their arguments
-                var movearms =function(arg1,arg2,correct){
+                       arg1 = args1[Math.floor(Math.random() * args1.length)]
+                       arg2 = args2[Math.floor(Math.random() * args2.length)]
+                       correct = choices[Math.floor(Math.random() * choices.length)]
                        arg1.regX = 40;
                        arg1.regY = 35;
                        arg2.regX=40;
@@ -221,7 +258,7 @@ stage.update();
                                 stage.removeChild(correct);
                                 stage.addChild(correct);
                                 createjs.Tween.get(correct, {loop:false})
-
+                                        
 
                                         .wait(2500)
                                         .call(turngreen)
@@ -253,6 +290,7 @@ stage.update();
 
                                         .call(turnback)
                                         .call(lightbutton)
+                                        
 
 
                         function turngreen() {
@@ -265,17 +303,18 @@ stage.update();
                                  background.graphics.beginFill("#1a8cff").drawRoundRect(0, 0, 150, 60, 10);
                         }
 
-
-
-
-                };
                         
+                        
+
+                
+                    
+                     
                        
 
 
 
                         //choose random arguments, place them in "apply" box
-                        movearms(args1[Math.floor(Math.random() * args1.length)],args2[Math.floor(Math.random() * args2.length)],t2);
+                    
 
 
 
@@ -283,9 +322,98 @@ stage.update();
                
 
         }
-        next();
+        var intro = function(){
+                var background = new createjs.Shape();
+                background.name = "background";
+                background.graphics.beginFill("#1a8cff").drawRoundRect(0, 0, 150, 60, 10);
+			
+                var label = new createjs.Text("Next", "bold 24px Arial", "#FFFFFF");
+                label.name = "label";
+                label.textAlign = "center";
+                label.textBaseline = "middle";
+                label.x = 150/2;
+                label.y = 60/2;
+                var instruct = new createjs.Container();
+                instruct.name = "button";
+                instruct.x = 450;
+                instruct.y = 735;
+                instruct.addChild(background, label);
+                instruct.on("click", function(){intro2(instruct,pressnext)});
+                stage.addChild(instruct);
+                        
+                var instructions = new createjs.Text("Welcome the the Concept Factory. Here, we put things together!", "bold 30px Arial","white");
+                instructions.x = 30;
+                instructions.y = 200;
+                var pressnext = new createjs.Text("Press Next to continue.","20px Arial","black");
+                pressnext.x = 100;
+                pressnext.y = 250;
+                stage.addChild(pressnext);
+                instructions.shadow= new createjs.Shadow("black",2,2,0);
+                var robot = new createjs.Bitmap("images/robot.png");
+                robot.x = 650;
+                robot.y = 350;
+                robot.scaleX=1.2;
+                robot.scaleY=1.2;
+                stage.addChild(robot);
+                stage.addChild(instructions);
+                
+                
+           
+      
+        
+        };
+        
+        var intro2 = function(instruct,pressnext){
+                
+                
+                var instructions = new createjs.Text("Here is our special box. \nWhen we put two objects into the box, \nthey transform to create something new!", "20px Arial","black");
+                instructions.x = 300;
+                instructions.y = 350;
+                
+                stage.removeChild(pressnext);
+                stage.addChild(instructions);
+                stage.addChild(box);
+                gear.x = 90;
+                gear.y = 350;
+                gear2.x = 90;
+                gear2.y = 410;
+                gear3.x = 225;
+                gear3.y = 480
+                
+                stage.addChild(gear);
+                stage.addChild(gear2);
+                stage.addChild(gear3);
+                 
+                box.x=50;
+                box.y = 320;
+                instruct.on("click",function(){intro3(instruct)});
+        
+        
+        }
+        var intro3 = function(instruct){
+                
+                var instructions = new createjs.Text("And here is our special equipment!", "20px Arial","black");
+                instructions.x = 300;
+                instructions.y = 450;
+                stage.addChild(instructions);
+                t1.y = 490;
+                t1.x = 300;
+                t2.x=390;
+                t3.x=480;
+                t2.y=490;
+                t3.y=490;
+                stage.addChild(t1,t2,t3);
+                instruct.on("click",next);
+        
+        
+        }
+        //the first trial
+        intro();
+       
+        
+        
         function handleClick(evt){
-                setTimeout(next,1000);
+               next();
                 
         }
         
